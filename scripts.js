4e1b3560398a382988192d42b8552c81ea75a232
samkessaram <samkessaram@gmail.com>
2017-02-16 06:20:47 +0000

    reworked replacement function

--- scripts.js
+++ scripts.js
@@ -1,45 +1,55 @@
-function repealAndReplace(node){
-  node.innerHTML = node.innerHTML.replace(/Donald\sJ\.?\sTrump/g,'Stephen K. Bannon')
-  node.innerHTML = node.innerHTML.replace(/Donald\sJohn\sTrump/g,'Stephen Kevin Bannon')
-  node.innerHTML = node.innerHTML.replace(/Donald\sTrump/g,'Steve Bannon')
-  node.innerHTML = node.innerHTML.replace(/\sTrump/g,' Bannon')
-  node.innerHTML = node.innerHTML.replace(/@realDonaldTrump/g,'@reallySteveBannon')
-  node.innerHTML = node.innerHTML.replace(/>Trump\s/g,'>Bannon ')
-  node.innerHTML = node.innerHTML.replace(/Trump\s/g,'Bannon ')
+function repealAndReplace(nodes){
+  for (var i = 0; i < nodes.length; i++) {
+    nicks.forEach(function(nick){
+      nodes[i].innerHTML = nodes[i].innerHTML.replace(nick[0], nick[1])
+    })
+  }
+
+  // node.innerHTML = node.innerHTML.replace(/Donald\sJ\.?\sTrump/g,'Stephen K. Bannon')
+  // node.innerHTML = node.innerHTML.replace(/Donald\sJohn\sTrump/g,'Stephen Kevin Bannon')
+  // node.innerHTML = node.innerHTML.replace(/Donald\sTrump/g,'Steve Bannon')
+  // node.innerHTML = node.innerHTML.replace(/\sTrump/g,' Bannon')
+  // node.innerHTML = node.innerHTML.replace(/@realDonaldTrump/g,'@reallySteveBannon')
+  // node.innerHTML = node.innerHTML.replace(/>Trump\s/g,'>Bannon ')
+  // node.innerHTML = node.innerHTML.replace(/Trump\s/g,'Bannon ')
 }
 
 function checkStorage(callback){
   console.log('Checking storage...')
   chrome.storage.local.get(null, function(resp){
-    callback(resp['clean_page'])
+    console.log(resp)
+    callback(resp)
   })
-}
-
-function getNodes(){
-  return document.querySelectorAll('a, p, h1, h2, h3, h4, h5, h6, figcaption, span');
 }
 
 function makeTheInternetGreat(){
   checkStorage(getSetNodes);
 }
 
-function getSetNodes(page){
-  if ( !page ){
+function getSetNodes(resp){
+  var pageMatch = resp['href'] === window.location.href
+  if ( !pageMatch ){
+
+    console.log('Saving clean page...')
+
     chrome.storage.local.set({
-      'clean_page': document.body.innerHTML
+      'clean_page': document.body.innerHTML,
+      'href': window.location.href
     })
-    nodes = getNodes()
-    for (var i = 0; i < nodes.length; i++) {
-      if ( nodes[i].innerHTML.indexOf('Trump') > -1){
-        repealAndReplace(nodes[i])
-      }
-    }
+
+    console.log('Replacing nodes...')
+
+    var nodes = document.querySelectorAll('a, p, h1, h2, h3, h4, h5, h6, figcaption, span');
+    repealAndReplace(nodes)
   } else {
-    console.log('page reset');
-    document.body.innerHTML = page;
-    chrome.storage.local.set({
-      'clean_page': ''
-    })
+
+    console.log('Resetting page...');
+
+    document.body.innerHTML = resp['clean_page'];
+
+    console.log('Clearing storage...')
+
+    chrome.storage.local.clear()
   }
 }
 
@@ -47,6 +57,10 @@ function getSetNodes(page){
 
 makeTheInternetGreat()
 
-
-// Sean 'CAN I FINISH???' Spicer
-// Donald 'Pussy-Grabbin'
+var nicks = [
+  [/(\bPresident\b\s)?Donald\sTrump/g,'Pathological Liar Donald Trump'],
+  [/Steve Bannon/g,'Darth Bannon'],
+  [/Sean Spicer/g,'Balding, Angry Melissa McCarthy'],
+  [/Justin Trudeau/g,'Justin \'Joe\' Trudeau'],
+  [/Reince Preibus/g, 'Reincelstiltskin']
+]
